d7ce6054ed607e2a7ec7445cf09246a95ec80410
Gustav Rasmussen <gcr84@hotmail.com>
2024-11-11 19:29:39 +0100

    chore: add reservoir_sample

--- src/prime.py
+++ src/prime.py
@@ -3,6 +3,13 @@ import math
 
 
 def is_prime(n: int) -> bool:
+    """Check if a given integer is a prime number.
+
+    :param n: _description_
+    :type n: int
+    :return: _description_
+    :rtype: bool
+    """
     for k in range(2, int(math.sqrt(n)) + 1):
         if n % k == 0:
             return False
